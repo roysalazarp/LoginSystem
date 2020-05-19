bc30fc66b3e452b885bb851f128d3077f3a6d2dd
roysalazarp <51371955+roysalazarp@users.noreply.github.com>
2020-05-19 18:07:31 +0300

    Update init.php

--- core/init.php
+++ core/init.php
@@ -4,7 +4,7 @@ session_start();
 
 $GLOBALS['config'] = array (
     'mysql' => array(
-        'host' => '127.0.0.1',
+        'host' => 'https://loginsystemphp.herokuapp.com/',
         'username' => 'root',
         'password' => '',
         'db' => 'LoginSystem'
